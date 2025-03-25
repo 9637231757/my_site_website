6d4478321f6c232dd3feaf12aa01af310fd37c3b
9637231757 <er.rohangaidhani@gmail.com>
2025-03-25 18:18:45 +0530

    adding models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import post, Author, Tag
+
 # Register your models here.
+admin.site.register(post)
+admin.site.register(Author)
+admin.site.register(Tag)
