ea8f1f2ca59948e053031cc586453743017a6627
9637231757 <er.rohangaidhani@gmail.com>
2025-03-26 17:55:12 +0530

    moved dummy data into the database

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,20 @@ from django.core.validators import MinLengthValidator
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
     
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
 
 class post(models.Model):
     title = models.CharField(max_length=150)
--- blog/views.py
+++ blog/views.py
@@ -3,67 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 all_posts = [
-    {  
-        "slug": "hike-in-the-mountains",  # This is a string for the post slug
-        "image": "Mountains.jpg",  # This is a string for the image file name
-        "author": "Rohan Gaidhani",  # This is a string for the author's name
-        "date": date(2025, 3, 13),  # This is a datetime object, assuming you've imported `date` from `datetime`
-        "title": "Mountain Hiking",  # This is a string for the title of the post
-        "excerpt": "There's nothing like the views you get while hiking in the Mountains! And I wasn't even prepared for what happened while I was enjoying the view!",  # This is a string for the post excerpt
-        "content": """Trekking in Maharashtra: A Journey Through the Western Ghats
-            Maharashtra, a state located on the western coast of India, is blessed
-            with a diverse landscape that offers a perfect escape for nature enthusiasts
-            adventure seekers, and trekkers alike. Known for its rugged hills, lush green
-            valleys, forts, waterfalls, and serene lakes, Maharashtra boasts some of the most
-            captivating trekking destinations in India. Whether you're a seasoned trekker or a
-            beginner, the state offers trails that cater to all levels of experience."""  # This is a multi-line string for the post content
-    },
-
-
     
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Rohan Gaidhani",
-        "date": date(2025, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-         
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Rohan Gaidhani",
-        "date": date(2025, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
 ] 
 
 def get_date(post):
