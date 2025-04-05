61ada96db6d19121737baaad953a5f3fcab3b605
9637231757 <er.rohangaidhani@gmail.com>
2025-04-06 02:28:41 +0530

    updated migrations and created forms

--- blog/models.py
+++ blog/models.py
@@ -1,33 +1,3 @@
-"""from django.db import models
-from django.core.validators import MinLengthValidator
-
-# Create your models here.
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    
-    def __str__(self):
-        return self.name
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email_address = models.EmailField()
-    
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-    
-    def __str__(self):
-        return self.full_name()
-
-class post(models.Model):
-    title = models.CharField(max_length=150)
-    excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
-    date = models.DateField(auto_now = True)
-    slug = models.SlugField(unique=True, db_index=True)
-    content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts",null=True, blank=True)  """
 # updated models.py file     
 from django.db import models
 from django.core.validators import MinLengthValidator
@@ -54,7 +24,6 @@ class Post(models.Model):  # Capitalized
     excerpt = models.CharField(max_length=200)
     # Consider using ImageField if storing actual images:
     image = models.ImageField(upload_to='uploads/posts/', null=True, blank=True)
-    #image = models.ImageField(upload_to="posts",null=True)
     date = models.DateField(auto_now_add=True)  # Changed to auto_now_add
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
@@ -73,8 +42,9 @@ class Post(models.Model):  # Capitalized
     class Meta:
         ordering = ['-date']  # Default ordering by date descending    
 
-class comment(models.Model):
+class Comment(models.Model):
     user_name = models.CharField(max_length=120)
     user_email = models.EmailField()
-    text = models.TextField(max_length=300)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commets")         
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")          
