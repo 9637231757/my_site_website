6af5c2b3ef679ffe15c069a9bb9f1ca412546b1e
9637231757 <er.rohangaidhani@gmail.com>
2025-04-20 22:20:00 +0530

    updated

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jkza#-ot)ehl^3^))(#qag5!fbo2%4($0d!*dl--!5_m8%$kul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = [True]
 
 ALLOWED_HOSTS = []
+
+
+
 
 
 # Application definition
--- my_site/urls.py
+++ my_site/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("blog/", include(blog_urls)),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
